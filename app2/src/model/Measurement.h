ad3a9a4dcc6980508f2ad185dd2b61b12ffa026d
Prevot Jade <jade.prevot@insa-lyon.fr>
2021-05-15 17:30:16 +0000

    Update Measurement.h

--- app2/src/model/Measurement.h
+++ app2/src/model/Measurement.h
@@ -1,24 +1,84 @@
+/*************************************************************************
+                           Measurement  -  description
+                             -------------------
+    beginning                : $07/05/2021$
+    copyright            : (C) $2021$ by $B3204 and B3025 $
+    e-mail               : $adrien.jaillet@insa-lyon.fr / william.jean@insa-lyon.fr / matheus.de-barros-silva@insa-lyon.fr
+                            brandon.da-silva-alves@insa-lyon.fr / jade.prevot@insa-lyon.fr$
+*************************************************************************/
+
+//---------- Interface of <Measurement> (file Measurement.h) ----------------
 #ifndef Measurement_H
 #define Measurement_H
 
+//--------------------------------------------------- Used Interfaces
 #include <string>
 #include "Date.h"
 #include "Attribute.h"
 #include "../factory/Reader.h"
 using namespace std;
 
+//------------------------------------------------------------- Constants
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Role of <Measurement>
+//
+//
+//------------------------------------------------------------------------
+
 class Measurement {
+
+//----------------------------------------------------------------- PUBLIC
 	public:
-		Measurement(const MeasurementData&);
-		Measurement(const MeasurementData&, const AttributeData&);
+	//----------------------------------------------------- Public methods
+    // type Method ( parameters list );
+    // How to use :
+    //
+    // Precondition :
+    //
+
+	//------------------------------------------------- Operators overloading
+
 		bool operator<(const Measurement&) const;
+		// How to use :
+    	//
+    	// Precondition :
+    	//
+
+	//-------------------------------------------- Constructors - destructor
+		Measurement(const MeasurementData&);
+		// How to use :
+    	//
+    	// Precondition :
+    	//
+
+		Measurement(const MeasurementData&, const AttributeData&);
+		// How to use :
+    	//
+    	// Precondition :
+    	//
+
 	friend ostream& operator<<(std::ostream&, const Measurement&);
 	friend class Controller;
+
+//------------------------------------------------------------------ PROTECTED
 	protected:
+	//----------------------------------------------------- Protected Methods
+
+	//----------------------------------------------------- Protected Attributes
+
+//------------------------------------------------------------------ PRIVATE
 	private:
+	//----------------------------------------------------- Private Methods
+
+	//----------------------------------------------------- Private Attributes
 		float value;
 		Date date;
 		Attribute attribute;
 };
 
-#endif
+//-------------------------------- Other definitions depending on <Measurement>
+
+#endif // Measurement_H
