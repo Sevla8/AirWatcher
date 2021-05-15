aaf55d74a2f20917e71a19292ec2cf0031db1a21
Prevot Jade <jade.prevot@insa-lyon.fr>
2021-05-15 17:06:09 +0000

    Update GovernmentAgency.cpp

--- app2/src/model/GovernmentAgency.cpp
+++ app2/src/model/GovernmentAgency.cpp
@@ -1,7 +1,46 @@
+/*************************************************************************
+                           GovernmentAgency  -  description
+                             -------------------
+    beginning                : $07/05/2021$
+    copyright            : (C) $2021$ by $B3204 and B3025 $
+    e-mail               : $adrien.jaillet@insa-lyon.fr / william.jean@insa-lyon.fr / matheus.de-barros-silva@insa-lyon.fr
+                            brandon.da-silva-alves@insa-lyon.fr / jade.prevot@insa-lyon.fr$
+*************************************************************************/
+
+//---------- Implementation of <GovernmentAgency> (file GovernmentAgency.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include of system files
 #include <iostream>
 using namespace std;
 
+//------------------------------------------------------ Include of local files
 #include "GovernmentAgency.h"
 #include "User.h"
 
-GovernmentAgency::~GovernmentAgency() {}
+//------------------------------------------------------------- Constants
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Public Methods
+// type GovernmentAgency::Method ( Parameters list )
+// Algorithm :
+//
+//{
+//} //----- End of Method
+
+//-------------------------------------------- constructors - destructor
+
+GovernmentAgency::~GovernmentAgency() 
+// Algorithm :
+//
+{
+
+} //----- End of ~GovernmentAgency
+
+//------------------------------------------------------------------ PROTECTED
+
+//----------------------------------------------------- Protected Methods
+
+//------------------------------------------------------------------ PRIVATE
