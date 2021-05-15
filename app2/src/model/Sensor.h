bddd450628ac50895c81784af1110e4a7ee3b0b9
Prevot Jade <jade.prevot@insa-lyon.fr>
2021-05-15 19:20:13 +0000

    Update Sensor.h

--- app2/src/model/Sensor.h
+++ app2/src/model/Sensor.h
@@ -1,6 +1,17 @@
+/*************************************************************************
+                           Sensor  -  description
+                             -------------------
+    beginning                : $07/05/2021$
+    copyright            : (C) $2021$ by $B3204 and B3025 $
+    e-mail               : $adrien.jaillet@insa-lyon.fr / william.jean@insa-lyon.fr / matheus.de-barros-silva@insa-lyon.fr
+                            brandon.da-silva-alves@insa-lyon.fr / jade.prevot@insa-lyon.fr$
+*************************************************************************/
+
+//---------- Interface of <Sensor> (file Sensor.h) ----------------
 #ifndef Sensor_H
 #define Sensor_H
 
+//--------------------------------------------------- Used Interfaces
 #include <string>
 #include <list>
 #include <iostream>
@@ -10,15 +21,63 @@
 
 using namespace std;
 
+//------------------------------------------------------------- Constants
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Role of <Sensor>
+//
+//
+//------------------------------------------------------------------------
+
 class Sensor {
+
+//----------------------------------------------------------------- PUBLIC
 	public:
-		Sensor(const SensorData&, const list<MeasurementData>&, const set<AttributeData>&);
-		Sensor(const SensorData&, const UserData&, const list<MeasurementData>&, const set<AttributeData>&);
+	//----------------------------------------------------- Public methods
+    // type Method ( parameters list );
+    // How to use :
+    //
+    // Precondition :
+    //
+
+	//------------------------------------------------- Operators overloading
+
 		bool operator<(const Sensor&) const;
+		// How to use :
+    	//
+    	// Precondition :
+    	//
+
+	//-------------------------------------------- Constructors - destructor
+
+		Sensor(const SensorData&, const list<MeasurementData>&, const set<AttributeData>&);
+		// How to use :
+    	//
+    	// Precondition :
+    	//
+
+		Sensor(const SensorData&, const UserData&, const list<MeasurementData>&, const set<AttributeData>&);
+		// How to use :
+    	//
+    	// Precondition :
+    	//
+		
 	friend ostream& operator<<(std::ostream&, const Sensor&);
 	friend class Controller;
+
+//------------------------------------------------------------------ PROTECTED
 	protected:
+	//----------------------------------------------------- Protected Methods
+
+	//----------------------------------------------------- Protected attributes
+
+//------------------------------------------------------------------ PRIVATE
 	private:
+	//----------------------------------------------------- Private Methods
+
+	//----------------------------------------------------- Private Attributes
 		string id;
 		float latitude;
 		float longitude;
@@ -27,4 +86,6 @@ class Sensor {
 		list<Measurement> measurements;
 };
 
-#endif
+//-------------------------------- Other definitions depending on <Sensor>
+
+#endif // Sensor_H
