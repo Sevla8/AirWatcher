2c95c0346e29b74bf9598271622115b18389b8e0
Prevot Jade <jade.prevot@insa-lyon.fr>
2021-05-15 19:23:26 +0000

    Update Sensor.cpp

--- app2/src/model/Sensor.cpp
+++ app2/src/model/Sensor.cpp
@@ -1,12 +1,43 @@
+/*************************************************************************
+                           Sensor  -  description
+                             -------------------
+    beginning                : $07/05/2021$
+    copyright            : (C) $2021$ by $B3204 and B3025 $
+    e-mail               : $adrien.jaillet@insa-lyon.fr / william.jean@insa-lyon.fr / matheus.de-barros-silva@insa-lyon.fr
+                            brandon.da-silva-alves@insa-lyon.fr / jade.prevot@insa-lyon.fr$
+*************************************************************************/
+
+//---------- Implementation of <Sensor> (file Sensor.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include of system files
 #include <iostream>
 #include <sstream>
 using namespace std;
 
+//------------------------------------------------------ Include of local files
 #include "Sensor.h"
 #include "PrivateUser.h"
 #include "../factory/Reader.h"
 
-ostream& operator<<(std::ostream& os, const Sensor& s) {
+//------------------------------------------------------------- Constants
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Public Methods
+// type Sensor::Method ( Parameters list )
+// Algorithm :
+//
+//{
+//} //----- End of Method
+
+//------------------------------------------------- Operators overloadinf
+
+ostream& operator<<(std::ostream& os, const Sensor& s) 
+// Algorithm :
+//
+{
 	os << s.id << ";" << s.latitude << ";" << s.longitude << ";";
 
 	PrivateUser* pu = dynamic_cast<PrivateUser*>(s.user);
@@ -19,13 +50,20 @@ ostream& operator<<(std::ostream& os, const Sensor& s) {
 	}
 
 	return os;
-}
+} //----- End of operator <<
 
-bool Sensor::operator<(const Sensor& s) const {
+bool Sensor::operator<(const Sensor& s) const 
+// Algorithm :
+//
+{
 	return id < s.id;
-}
+} //----- End of operator <
+
+//-------------------------------------------- constructors - destructor
 
 Sensor::Sensor(const SensorData& sd, const list<MeasurementData>& md, const set<AttributeData>& ad) :
+// Algorithm :
+//
 	id(sd.id),
 	latitude(sd.latitude),
 	longitude(sd.longitude) {
@@ -36,9 +74,11 @@ Sensor::Sensor(const SensorData& sd, const list<MeasurementData>& md, const set<
 	}
 
 	user = new User();
-}
+} //----- End of Sensor
 
 Sensor::Sensor(const SensorData& sd, const UserData& ud, const list<MeasurementData>& md, const set<AttributeData>& ad) :
+// Algorithm :
+//
 	id(sd.id),
 	latitude(sd.latitude),
 	longitude(sd.longitude) {
@@ -50,4 +90,12 @@ Sensor::Sensor(const SensorData& sd, const UserData& ud, const list<MeasurementD
 	}
 
 	user = new PrivateUser(ud);
-}
+} //----- End of Sensor
+
+//------------------------------------------------------------------ PROTECTED
+
+//----------------------------------------------------- Protected Methods
+
+//------------------------------------------------------------------ PRIVATE
+
+//----------------------------------------------------- Private Methods
