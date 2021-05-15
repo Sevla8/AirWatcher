56f7aa76fb98f2df6d213117bac66f5b9ad0ea67
Prevot Jade <jade.prevot@insa-lyon.fr>
2021-05-15 16:44:06 +0000

    Update Cleaner.h

--- app2/src/model/Cleaner.h
+++ app2/src/model/Cleaner.h
@@ -1,6 +1,17 @@
+/*************************************************************************
+                           Cleaner  -  description
+                             -------------------
+    beginning                : $07/05/2021$
+    copyright            : (C) $2021$ by $B3204 and B3025 $
+    e-mail               : $adrien.jaillet@insa-lyon.fr / william.jean@insa-lyon.fr / matheus.de-barros-silva@insa-lyon.fr
+                            brandon.da-silva-alves@insa-lyon.fr / jade.prevot@insa-lyon.fr$
+*************************************************************************/
+
+//---------- Interface of <Cleaner> (file Cleaner.h) ----------------
 #ifndef Cleaner_H
 #define Cleaner_H
 
+//--------------------------------------------------- Used Interfaces
 #include <string>
 #include "Date.h"
 #include "Provider.h"
@@ -8,13 +19,54 @@
 
 using namespace std;
 
+//------------------------------------------------------------- Constants
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Role of <Cleaner>
+//
+//
+//------------------------------------------------------------------------
+
 class Cleaner {
+
+//----------------------------------------------------------------- PUBLIC
 	public:
-		Cleaner(const CleanerData&, const ProviderData&);
+	//----------------------------------------------------- Public methods
+    // type Method ( parameters list );
+    // How to use :
+    //
+    // Precondition :
+    //
+
+	//------------------------------------------------- Operators overloading
 		bool operator<(const Cleaner&) const;
+		// How to use :
+    	//
+    	// Precondition :
+    	//
+
+	//-------------------------------------------- Constructors - destructor
+		Cleaner(const CleanerData&, const ProviderData&);
+		// How to use :
+    	//
+    	// Precondition :
+    	//
+
 	friend ostream& operator<<(std::ostream&, const Cleaner&);
+
+//------------------------------------------------------------------ PROTECTED
 	protected:
+	//----------------------------------------------------- Protected Methods
+
+	//----------------------------------------------------- Protected Attributes
+
+	//------------------------------------------------------------------ PRIVATE
 	private:
+	//----------------------------------------------------- Private Methods
+
+	//----------------------------------------------------- Private Attributes
 		string id;
 		float latitude;
 		float longitude;
@@ -23,4 +75,6 @@ class Cleaner {
 		Provider provider;
 };
 
-#endif
+//-------------------------------- Other definitions depending on <Cleaner>
+
+#endif // Cleaner_H
