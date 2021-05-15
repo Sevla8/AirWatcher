449ba33a332060624f657b4281d21b6c204b22a8
Prevot Jade <jade.prevot@insa-lyon.fr>
2021-05-15 16:55:13 +0000

    Update Date.h

--- app2/src/model/Date.h
+++ app2/src/model/Date.h
@@ -1,22 +1,88 @@
+/*************************************************************************
+                           Date  -  description
+                             -------------------
+    beginning                : $07/05/2021$
+    copyright            : (C) $2021$ by $B3204 and B3025 $
+    e-mail               : $adrien.jaillet@insa-lyon.fr / william.jean@insa-lyon.fr / matheus.de-barros-silva@insa-lyon.fr
+                            brandon.da-silva-alves@insa-lyon.fr / jade.prevot@insa-lyon.fr$
+*************************************************************************/
+
+//---------- Interface of <Date> (file Date.h) ----------------
 #ifndef Date_H
 #define Date_H
 
+//--------------------------------------------------- Used Interfaces
 #include <string>
 #include <iostream>
 
 using namespace std;
 
+//------------------------------------------------------------- Constants
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Role of <Date>
+//
+//
+//------------------------------------------------------------------------
+
 class Date {
+
+//----------------------------------------------------------------- PUBLIC
 	public:
-		Date(int, int, int, int, int, int);
-		Date();
+	//----------------------------------------------------- Public methods
+    // type Method ( parameters list );
+    // How to use :
+    //
+    // Precondition :
+    //
+
+	//------------------------------------------------- Operators overloading
+
 		bool operator<(const Date&) const;
+		// How to use :
+    	//
+    	// Precondition :
+    	//
 		bool operator==(const Date&) const;
+		// How to use :
+    	//
+    	// Precondition :
+    	//
+
 		bool operator<=(const Date&) const;
+		// How to use :
+    	//
+    	// Precondition :
+    	//
+
+	//-------------------------------------------- Constructors - destructor
+
+		Date(int, int, int, int, int, int);
+		// How to use (copy constructor): Instantiate and initialize a new instace of Date having the attributes affected by the
+    	//                                  corresponding parameters
+
+		Date();
+		// How to use : Initialize a new date having all the values as the default Date : 01-01-1970 00:00:00
+    	//
+    	// Precondition :
+    	//
+		
 	friend std::istream& operator>>(std::istream&, Date&);
 	friend ostream& operator<<(std::ostream&, const Date&);
+
+//------------------------------------------------------------------ PROTECTED
 	protected:
+	//----------------------------------------------------- Protected Methods
+
+	//----------------------------------------------------- Protected Attributes
+
+//------------------------------------------------------------------ PRIVATE
 	private:
+	//----------------------------------------------------- Private Methods
+
+	//----------------------------------------------------- Private Attributes
 		int year;
 		int month;
 		int day;
@@ -25,4 +91,6 @@ class Date {
 		int second;
 };
 
-#endif
+//-------------------------------- Other definitions depending on <Date>
+
+#endif // Date_H
