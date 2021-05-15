36e5c1ea6bccce1fa40ac2405a508c3c3af008b1
Prevot Jade <jade.prevot@insa-lyon.fr>
2021-05-15 12:17:55 +0000

    Update Controller.h

--- app2/src/control/Controller.h
+++ app2/src/control/Controller.h
@@ -1,19 +1,86 @@
+/*************************************************************************
+                           Controller  -  description
+                             -------------------
+    beginning                : $07/05/2021$
+    copyright            : (C) $2021$ by $B3204 and B3025 $
+    e-mail               : $adrien.jaillet@insa-lyon.fr / william.jean@insa-lyon.fr / matheus.de-barros-silva@insa-lyon.fr
+                            brandon.da-silva-alves@insa-lyon.fr / jade.prevot@insa-lyon.fr$
+*************************************************************************/
+
+//---------- Interface of <Controller> (file Controller.h) ----------------
 #ifndef Controller_H
 #define Controller_H
 
+//--------------------------------------------------- Used Interfaces
 #include <string>
 #include "../model/Model.h"
 
+//------------------------------------------------------------- Constants
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Role of <Controller>
+//
+//
+//------------------------------------------------------------------------
+
 class Controller {
+//----------------------------------------------------------------- PUBLIC
 	public:
-		Controller();
-		virtual ~Controller();
+	//----------------------------------------------------- Public methods
+    // type Method ( parameters list );
+    // How to use :
+    //
+    // Precondition :
+    //
+
 		Model getModel() const;
+		// type Method ( parameters list );
+    	// How to use :
+    	//
+    	// Precondition :
+    	//
+
 		string analyseAirQualityInCircularArea(float, float, float, const Date&, const Date&);
+		// type Method ( parameters list );
+    	// How to use :
+    	//
+		// Precondition :
+    	//
+
+	//------------------------------------------------- Operators overloading
+
+	//-------------------------------------------- Constructors - destructor
+		Controller();
+		// How to use : Constructs a Controller
+    	//
+    	// Precondition :
+    	//
+
+		virtual ~Controller();
+		// How to use :
+    	//
+    	// Precondition :
+    	//
+		
 	friend ostream& operator<<(std::ostream&, const Controller&);
+
+//------------------------------------------------------------------ PROTECTED
 	protected:
+	//----------------------------------------------------- Protected Methods
+
+	//----------------------------------------------------- Protected Attributes
+
+//------------------------------------------------------------------ PRIVATE
 	private:
+	//----------------------------------------------------- Private Methods
+
+	//----------------------------------------------------- Private Attributes
+
 		Model model;
 };
 
-#endif
+//-------------------------------- Other definitions depending on <Controller>
+
+#endif // Controller_H
